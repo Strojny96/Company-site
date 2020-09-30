473ca3fda3d976ba92c4538ab3d9b6518749e39c
Piotrek Strojny <p.strojny@fivedottwelve.com>
2020-09-30 12:26:03 +0200

    Form button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
 
 let tittle = document.querySelector('.open-title');
 let navbar = document.querySelector('.nav');
+let formButton = document.querySelector('.form-button');
+let modalBg = document.querySelector('.modal-bg');
+let modalClose = document.querySelector('.modal-close');
 let arrows = document.querySelector('.arrows');
 let wrapper = document.querySelector('.wrapper');
+let main = document.querySelector('main');
+let footer = document.querySelector('footer');
 
 const headerChange = () => {
      tittle.classList.add('site-title');
      tittle.classList.remove('open-title');
      navbar.classList.add('nav-roll');
+     footer.classList.add('load');
  };
 
+formButton.addEventListener('click', () => {
+    modalBg.classList.add('bg-active');
+})
+
+modalClose.addEventListener('click', () => {
+    modalBg.classList.remove('bg-active');
+})
 arrows.addEventListener("mouseenter", () => {
     wrapper.scrollIntoView();
 });
